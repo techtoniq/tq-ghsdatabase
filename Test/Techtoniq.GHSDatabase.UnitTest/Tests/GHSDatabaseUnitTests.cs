c8d2968acdc1f6923c7153f2c5c247468241efaf
Matt Daniels <matt@techtoniq.com>
2020-11-30 18:40:41 +0000

    Fixed failing test +semver: minor

--- Test/Techtoniq.GHSDatabase.UnitTest/Tests/GHSDatabaseUnitTests.cs
+++ Test/Techtoniq.GHSDatabase.UnitTest/Tests/GHSDatabaseUnitTests.cs
@@ -103,7 +103,7 @@ namespace Techtoniq.GHSDatabase.UnitTest
 
                 // Assert.
                 hazards.Should().NotBeNull();
-                hazards.Should().HaveCount(3);
+                hazards.Should().HaveCount(4);
             }
         }
     }
