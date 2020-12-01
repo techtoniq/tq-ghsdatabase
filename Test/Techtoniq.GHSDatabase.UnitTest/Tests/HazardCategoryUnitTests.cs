018ac19b7ed0cbb0e15b5daab066489922ec204c
Matt Daniels <silverelise@hotmail.com>
2020-12-01 10:10:00 +0000

    Resource test refactor and added HazardCategory tests

--- Test/Techtoniq.GHSDatabase.UnitTest/Tests/HazardCategoryUnitTests.cs
+++ Test/Techtoniq.GHSDatabase.UnitTest/Tests/HazardCategoryUnitTests.cs
@@ -15,15 +15,15 @@ namespace Techtoniq.GHSDatabase.UnitTest.Tests
     /// we pass the ordinal values out of the test cases and cast them back to an enum inside the tests
     /// </remarks>
     [TestFixture]
-    public class HazardClassUnitTests
+    public class HazardCategoryUnitTests
     {
-        public class HazardClassTestData
+        public class HazardCategoryTestData
         {
             public static IEnumerable TestCases
             {
                 get
                 {
-                    foreach(var e in Enum.GetValues(typeof(GHSDatabase.HazardClass)))
+                    foreach (var e in Enum.GetValues(typeof(GHSDatabase.HazardCategory)))
                     {
                         yield return new TestCaseData(e, "en");
                     }
@@ -31,20 +31,20 @@ namespace Techtoniq.GHSDatabase.UnitTest.Tests
             }
         }
 
-        [TestFixture(Description = "HazardClass enum tests")]
-        public class HazardClass
+        [TestFixture(Description = "HazardCategory enum tests")]
+        public class HazardCategory
         {
-            [TestCaseSource(typeof(HazardClassTestData), nameof(HazardClassTestData.TestCases))]
+            [TestCaseSource(typeof(HazardCategoryTestData), nameof(HazardCategoryTestData.TestCases))]
             public void When_KeyDefined_Then_ResourceExists(int ordinalValue, string cultureName)
             {
                 // Arrange.
 
-                var key = (GHSDatabase.HazardClass)ordinalValue;
+                var key = (GHSDatabase.HazardCategory)ordinalValue;
                 var cultureInfo = CultureInfo.CreateSpecificCulture(cultureName);
 
                 // Act.
 
-                var value = StringResourceManager.GetHazardClassString(key, cultureInfo);
+                var value = StringResourceManager.GetHazardCategoryString(key, cultureInfo);
 
                 // Assert.
 
