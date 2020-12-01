9536fc2d2a156f8fcf11aba3cddbbc675cf31193
Matt Daniels <silverelise@hotmail.com>
2020-12-01 06:14:05 +0000

    Skeleton test framework for resources.

---
+++ Test/Techtoniq.GHSDatabase.UnitTest/Tests/ResourcesUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Techtoniq.GHSDatabase.UnitTest.Tests
+{
+    /// <summary>
+    /// Development helper.
+    /// Checks that every key in the dictionary has a corresponding resource in the resource file.
+    /// </summary>
+    [TestFixture]
+    public class ResourcesUnitTests
+    {
+        [TestFixture(Description = "HazardClass enum tests")]
+        public class HazardClass
+        {
+            [Test]
+            public void When_KeyDefined_Then_ResourceExists()
+            {
+
+            }
+        }
+    }
+}
