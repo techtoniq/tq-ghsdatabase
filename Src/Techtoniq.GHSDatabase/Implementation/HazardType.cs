053624d653802480b98aa4f2b6221a884d5e1eba
Matt Daniels <matt@techtoniq.com>
2020-12-05 11:45:06 +0000

    HazardType and PCodeType

---
+++ Src/Techtoniq.GHSDatabase/Implementation/HazardType.cs
@@ -0,0 +1,10 @@
+﻿namespace Techtoniq.GHSDatabase
+{
+    internal enum HazardType
+    {
+        PhysicalHazards = 200,
+        HealthHazards = 300,
+        EnvironmentalHazards = 400,
+        AdditionalHazardStatements = 0,
+    }
+}
---
+++ Src/Techtoniq.GHSDatabase/Implementation/PCodeType.cs
@@ -0,0 +1,11 @@
+﻿namespace Techtoniq.GHSDatabase
+{
+    internal enum PCodeType
+    {
+        General = 100,
+        Prevention = 200,
+        Response = 300,
+        Storage = 400,
+        Dispoal = 500
+    }
+}
